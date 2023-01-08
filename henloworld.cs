8ee100edfb01e3a5080af39134fea06a359f376d
strawberrycrepecookieisbest <109152364+strawberrycrepecookieisbest@users.noreply.github.com>
2023-01-08 21:24:40 +0600

    Add files via upload
    модуль 3 (введение в С#) (strawberrycookieisthebestcookieinentiregame)

---
+++ henloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class henloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         //ihaveallcostumesonstrawberrycrepecookie
+        print("cut cut perm perm cut cut");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
